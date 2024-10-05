1c8e9b0d6d6ec5c65cfb6366aa36a61737afca77
robin <robin@DESKTOP-2FPU8J6.home>
2024-10-05 21:42:43 +0200

    Model

--- SoloProjects2ndSemesterJava2.61RK/src/Calculator/Calculator_Model.java
+++ SoloProjects2ndSemesterJava2.61RK/src/Calculator/Calculator_Model.java
@@ -1,5 +1,43 @@
 package Calculator;
 
 public class Calculator_Model {
+	
+private double result;
+private String operator;
+	    
+public CalculatorModel() {
+result = 0;
+operator = "";
+}
+	    
+// Methoden zur Durchführung von Berechnungen
+public void calculate(double input, String operator) {
+	switch (this.operator) {
+	 case "+":
+	                result += input;
+	                break;
+	            case "-":
+	                result -= input;
+	                break;
+	            case "*":
+	                result *= input;
+	                break;
+	            case "/":
+	                result /= input;
+	                break;
+	            default:
+	                result = input;
+	                break;
+	        }
+	        this.operator = operator;
+	    }
 
+	    public double getResult() {
+	        return result;
+	    }
+
+	    public void clear() {
+	        result = 0;
+	        operator = "";
+	    }
 }
